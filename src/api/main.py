b6227f3a79e16648d2c8d5f4f2bcd9d3ce832cf0
benjamin <benjaminschug@gmail.com>
2017-04-23 19:56:07 +0200

    Add option to run flask app directly

--- src/api/main.py
+++ src/api/main.py
@@ -8,8 +8,13 @@ app = Flask(__name__)
 
 db_credentials = os.environ['DB_CREDENTIALS']
 
+
 @app.route('/api/v1/stats')
 def stats():
     itemdb = ItemDB(db_credentials)
     stats = itemdb.get_stats()
     return json.dumps(stats)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
