dca34789748276a81c1542007b60c78706604369
benjamin <benjaminschug@gmail.com>
2017-05-01 17:23:24 +0200

    Support Legacy league and log warnings for unsupported leagues

--- src/constants/league.py
+++ src/constants/league.py
@@ -1,19 +1,28 @@
+from blessings import Terminal
+
 UNKNOWN = 0
 STANDARD = 1
 HARDCORE = 2
 BREACH = 3
 BREACH_HC = 4
+LEGACY = 5
+LEGACY_HC = 6
 
 LEAGUE_ID = {
     'Standard': STANDARD,
     'Hardcore': HARDCORE,
     'Breach': BREACH,
-    'Hardcore Breach': BREACH_HC
+    'Hardcore Breach': BREACH_HC,
+    'Legacy': LEGACY,
+    'Hardcore Legacy': LEGACY_HC
 }
 
 
 def get_id(league_name):
-    return LEAGUE_ID.get(league_name, 0)
+    league_id = LEAGUE_ID.get(league_name, UNKNOWN)
+    if league_id == UNKNOWN:
+        print(Terminal().bold_red("Unknown League: ", league_name))
+    return league_id
 
 
 def get_name(league_id):
